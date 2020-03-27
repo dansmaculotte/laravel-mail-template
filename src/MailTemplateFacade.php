63b1429f266a2ac262962e92303173df3f5f6e4b
Gaël Reyrol <me@gaelreyrol.dev>
2020-03-27 17:22:58 +0100

    chore: reference methods on facade with phpdoc

--- src/MailTemplateFacade.php
+++ src/MailTemplateFacade.php
@@ -5,7 +5,16 @@ namespace DansMaCulotte\MailTemplate;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \DansMaCulotte\MailTemplate\MailTemplate
+ * @see MailTemplate
+ * @method MailTemplate make()
+ * @method MailTemplate setFrom(string $name, string $email)
+ * @method MailTemplate setTemplate($template)
+ * @method MailTemplate setSubject(string $subject)
+ * @method MailTemplate setRecipient(string $name, string $email)
+ * @method MailTemplate setVariables(array $variables)
+ * @method MailTemplate setLanguage(string $language)
+ * @method array send()
+ * @method array toArray()
  */
 class MailTemplateFacade extends Facade
 {
